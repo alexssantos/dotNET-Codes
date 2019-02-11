e3b09e4ec1dc9381c82cdf36b04c8194f1c09e6a
Alex Santos <arkanjo.alex@gmail.com>
2019-02-11 13:27:50 -0200

    Criando Threads

--- Threads/Threads/Threads/Program.cs
+++ Threads/Threads/Threads/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threads
@@ -10,6 +11,13 @@ namespace Threads
 	{
 		static void Main(string[] args)
 		{
+			var work1 = new Work1();
+			var work2 = new Work2();
+			var thread1 = new Thread(() => work1.DoWork());
+			thread1.Start();
+			var thread2 = new Thread(() => work2.DoWork());
+			thread2.Start();
+			Console.ReadKey();
 		}
 	}
 }
