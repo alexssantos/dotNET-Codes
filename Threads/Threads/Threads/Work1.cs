e5ace1b772bfd4247a3274cc2758080f64f83436
Alex Santos <arkanjo.alex@gmail.com>
2019-02-11 13:26:13 -0200

    Work2 para discernir as threads

---
+++ Threads/Threads/Threads/Work1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Threads
+{
+	public class Work1
+	{
+		public void DoWork()
+		{
+			for (int i = 1; i < 1001; i++)
+				Console.WriteLine($"Thread 1 -> Iteração {i}");
+		}
+	}
+}
---
+++ Threads/Threads/Threads/Work2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+	public class Work2
+	{
+		public void DoWork()
+		{
+			for (int i = 1; i < 1001; i++)
+				Console.WriteLine($"Thread 2 -> Iteração {i}");
+		}
+	}
+}
