64c6f2cfdaf249cd25090fb1be5540e6a5fdc1e7
Alex Santos <arkanjo.alex@gmail.com>
2020-02-15 18:08:27 -0300

    Product controller

--- APIs/core3-ef-15min/Controllers/CategoryController.cs
+++ APIs/core3-ef-15min/Controllers/CategoryController.cs
@@ -22,21 +22,21 @@ namespace core3_ef_15min.Controllers
 
 		[HttpGet]
 		[Route("")]
-		public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+		public async Task<IActionResult> Get([FromServices] DataContext context)
 		{
 			var categories = await context.Categories.ToListAsync();
-			return categories;
+			return Ok(categories);
 		}
 
 		[HttpPost]
 		[Route("")]
-		public async Task<ActionResult<Category>> Post([FromServices] DataContext context, [FromBody] Category model)
+		public async Task<IActionResult> Post([FromServices] DataContext context, [FromBody] Category model)
 		{
 			if (ModelState.IsValid)
 			{
 				context.Categories.Add(model);
 				await context.SaveChangesAsync();
-				return model;
+				return Ok(model);
 			}
 			else 
 			{
---
+++ APIs/core3-ef-15min/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using core3_ef_15min.Data;
+using core3_ef_15min.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+
+namespace core3_ef_15min.Controllers
+{
+	[ApiController]
+	[Route("v1/products")]
+	public class ProductController : ControllerBase
+	{
+
+		//FIXME: Modo Antigo
+		// private DataContext _context;
+		// public ProductController(DataContext context)
+		// {
+		//     _context = context
+		// }
+
+		[HttpGet]
+		[Route("")]
+		public async Task<IActionResult> Get([FromServices] DataContext context)
+		{
+			var products = await context.Products
+				.Include(x => x.Category)
+				.ToListAsync();
+
+			return Ok(products);
+		}
+
+		[HttpGet]
+		[Route("{id:int}")]
+		public async Task<IActionResult> GetById([FromServices] DataContext context,int id)
+		{
+			var product = await context.Products
+				.Include(bean => bean.Category)
+				.AsNoTracking()
+				.FirstOrDefaultAsync(bean => bean.Id == id);
+			return Ok(product);
+		}
+
+		[HttpGet]
+		[Route("categories/{id:int}")]
+		public async Task<IActionResult> GetByCategory([FromServices] DataContext context, int id)
+		{
+			var products = await context.Products
+				.Include(bean => bean.Category)
+				.AsNoTracking()
+				.Where(bean => bean.Category.Id == id)
+				.ToListAsync();
+				
+			return Ok(products);
+		}
+
+		[HttpPost]
+		[Route("")]
+		public async Task<IActionResult> Post([FromServices] DataContext context, [FromBody] Product model)
+		{
+			if (ModelState.IsValid)
+			{
+				context.Products.Add(model);
+				await context.SaveChangesAsync();
+				return Ok(model);
+			}
+			else
+			{
+				return BadRequest(ModelState);
+			}
+		}
+	}
+}
