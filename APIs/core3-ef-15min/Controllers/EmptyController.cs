15ba85acfd32dcb304c51291a71bf0da71ccadbc
Alex Santos <arkanjo.alex@gmail.com>
2020-02-20 11:57:45 -0300

    - AbstractController - GetIp metodo

---
+++ APIs/core3-ef-15min/Controllers/EmptyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace core3_ef_15min.Controllers
+{
+	[Route("api/Empty")]
+	[ApiController]
+	public class EmptyController : AbstractApiController
+	{
+		public EmptyController()
+		{
+
+		}//constructor
+
+		[HttpGet]
+		[Route("catchIp")]
+		public ActionResult<string> CatchIp()
+		{
+			var ip = GetIp(Request);
+
+			return Ok(ip);
+		}
+
+	}
+}
